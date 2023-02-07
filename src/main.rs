47d31ba20a85da5d25f0512d1fb68cc9cee4a99d
PTFOPlayer <patryk041103@gmail.com>
2023-02-07 18:45:33 +0100

    final version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use itertools::Itertools;
-use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use std::fs;
 use std::process::exit;
 
@@ -87,24 +86,31 @@ fn main() {
     println!("workable pairs created: {}\n", pairs.len());
 
     let options = pairs
-        .par_iter()
+        .iter()
         .map(|t| {
             let (i, j) = t;
             let mut div = 1;
-            let mut n =  i128::abs((j*public[0]) - (i*public[1]));
-            while n > max {
-                n = n/div;
-                let mut inv = euc_lib::I128::euc_ext(n, *i).t;
-                if inv < 0 {
-                    inv += n;
-                }
-
-                let q = inv * public[0] % n;
-                if public[0] == (i * q) % n && public[1] == (j * q) % n {
-                    return (*i, *j, q, n);
-                }
+            let mut n =  i128::abs((i*public[1])- (j*public[0]));
+            
+            let mut n_c = n.clone();
+            while n_c > max {
                 div += 1;
+                n_c = n/div;
             }
+            if div == 1 {
+                return (0, 0, 0, 0);    
+            }
+
+            n = n/(div -1);
+            let mut inv = euc_lib::I128::euc_ext(n, *i).t;
+            if inv < 0 {
+                inv += n;
+            }
+            let q = inv * public[0] % n;
+            if public[0] == (i * q) % n && public[1] == (j * q) % n {
+                return (*i, *j, q, n);
+            }
+            
             return (0, 0, 0, 0);
         })
         .collect::<Vec<(i128, i128, i128, i128)>>();
@@ -115,38 +121,75 @@ fn main() {
         .collect::<Vec<&(i128, i128, i128, i128)>>();
     println!("working options created, {}, \n", options.len());
 
-    let estim = options
-        .par_iter()
+    let first_estim = options
+        .iter()
         .map(|t| {
             let (u0, u1, q, n) = t;
-            let mut count = 0;
-            let mut prev = u0+u1;
             let mut vec = vec![];
             vec.append(&mut vec![*u0]);
             vec.append(&mut vec![*u1]);
-            for i in 2..public.len() {
-                for j in prev..(*n) {
-                    if public[i] == (j * q) % n {
-                        count += 1;
-                        prev += j;
-                        vec.append(&mut vec![j]);
-                        break;
-                    }
+            if *n != 0 {
+                let mut q_inv = euc_lib::I128::euc_ext(*n, *q).t;
+                if q_inv < 0 {
+                    q_inv += n;
                 }
-            }
-            if count == size {
-                return (*q, *n, vec);
+                for i in 2..public.len() {
+                    let mut ui = (public[i] * q_inv) % n;
+                    if ui < 0 {
+                        ui += n;
+                    }
+                    sum += ui;
+                    vec.append(&mut vec![ui]);
+                }
+                return (*q,*n,vec);
             }
             return (0, 0, vec![]);
         })
         .collect::<Vec<(i128, i128, Vec<i128>)>>();
-
-    let mut estim = estim
+    let mut first_estim = first_estim
         .iter()
         .unique()
         .sorted()
         .collect::<Vec<&(i128, i128, Vec<i128>)>>();
-    estim.sort();
-    _ = estim.remove(0);
-    println!("estim: {}, {:?}", estim.len(), estim[0])
+    first_estim.sort();
+    _ = first_estim.remove(0);
+    println!("first estim: {} \n", first_estim.len());
+
+    for res in first_estim.clone() {
+        let mut v = res.2.clone();
+        v.sort();
+        println!("one of solutions Q:{} N:{} key:{:?} \n", res.0, res.1, v);
+    }
+
+    let sec_estim = first_estim.iter().map(|est|{
+        let v_o = est.2.clone();
+        let mut v_s = est.2.clone();
+        v_s.sort();
+        sum = 0;
+        for i in v_o.clone() {
+            sum += i;
+        }
+        if v_o == v_s && sum < est.1 {
+            return (est.0, est.1, est.2.clone());
+        } else {
+            return (0, 0, vec![]);
+        }
+    })
+    .collect::<Vec<(i128, i128, Vec<i128>)>>();
+    
+    let mut sec_estim = sec_estim
+        .iter()
+        .unique()
+        .sorted()
+        .collect::<Vec<&(i128, i128, Vec<i128>)>>();
+    sec_estim.sort();
+    sec_estim.remove(0);
+
+    println!("second estim: {} \n", sec_estim.len());
+    
+    println!("more correct solutions:");
+    for res in sec_estim {
+        println!("one of solutions Q:{} N:{} key:{:?} \n", res.0, res.1, res.2);
+    }
+    
 }
